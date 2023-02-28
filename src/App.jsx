8e71139230f51d86d5cf4cf5a34a906cbf0bb14b
Your Name <92824573+kyleehooley@users.noreply.github.com>
2023-02-28 10:17:53 -0600

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import "./index.css"
+
+import Counter from "./components/Counter"
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+export default App
